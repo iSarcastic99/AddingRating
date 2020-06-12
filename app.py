5a8ad5eeb3e3b99f28ef798df981cb01b6662488
Rahul Singh Sengar <rsflyedith@gmail.com>
2020-06-12 14:30:23 +0530

    Deleted ratingUpd file and editted it to Flask app

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-from ratingUpd import addRate
+from flask import Flask, request, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -9,16 +8,59 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/Rating',methods=['POST'])
 
 def addRating():
-    jobId = request.form['jobID']
+    isThere = False
+
+    df1 = pd.read_csv('rating.csv')
+    dfjson = df1.to_json(r'./endgameRating.json')
+    jobId = request.form['jobId']
     userId = request.form['userId']
     rating = request.form['rating']
-    print(userId," ", jobId," ", rating)
-    status = addRate(userId, jobId, rating)
+    print(userId, " ", jobId, " ", rating)
 
-    return jsonify(status)
+
+    try:
+        userFloat = float(userId)
+        jobFloat = float(jobId)
+        ratingFloat = float(rating)
+        print(userFloat, jobFloat, ratingFloat)
+
+        if (0 > ratingFloat or ratingFloat > 10):
+            invalid = "invalid"
+            return invalid
+
+    except ValueError:
+        error = "error"
+        return error
+
+    for index, row in df1.iterrows():
+        print(row['userID'], row['jobID'])
+        if str(row['jobID']) == str(jobId) and str(row['userID']) == str(userId):
+            row['rating'] = rating
+            isThere = True
+
+        # Creating the Second Dataframe using dictionary
+
+    if isThere != True:
+        df2 = pd.DataFrame({"userID": [userId],
+                            "jobID": [jobId],
+                            "rating": [rating]})
+
+        dff = df1.append(df2, ignore_index=True)
+        dff.to_csv(r'./rating.csv', index=False)
+        added = "added"
+        return render_template('index.html', DataStored="The rating has been stored")
+
+    else:
+
+        df1.to_csv(r'./rating.csv', index=False)
+        updated = "updated"
+        return updated
+
+
+    return render_template('index.html', DataStored="The rating has been stored")
 
 
 if __name__ == "__main__":
